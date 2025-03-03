dc5775f843b76a34c8a85e50cee31d2e973dc34c
benoliel <hugobeno@MacBook-Air-de-benoliel.local>
2025-03-03 11:28:42 +0100

    Ajout JUnit5, test coordonnées et Otetromino

---
+++ src/fr/eseo/e3/ppo/projet/blox/modele/pieces/tetrominos/OTetromino.java
@@ -0,0 +1,51 @@
+package fr.eseo.e3.ppo.projet.blox.modele.pieces.tetrominos;
+
+import fr.eseo.e3.ppo.projet.blox.modele.Coordonnees;
+import fr.eseo.e3.ppo.projet.blox.modele.Couleur;
+//import fr.eseo.e3.ppo.projet.blox.modele.Element;
+
+public class OTetromino {
+
+    private Element[] elements;
+
+    // Constructeur
+    public OTetromino(Coordonnees coordonnees, Couleur couleur) {
+        setElements(coordonnees, couleur);  // Remplir le tableau d'éléments
+    }
+
+    // Remplir le tableau d'éléments
+    protected void setElements(Coordonnees coordonnees, Couleur couleur) {
+        elements = new Element[4];
+
+        // Premier élément (élément de référence)
+        elements[0] = new Element(coordonnees, couleur);
+
+        // Deuxième élément : à droite
+        elements[1] = new Element(new Coordonnees(coordonnees.getAbscisse() + 1, coordonnees.getOrdonnee()), couleur);
+
+        // Troisième élément : en dessous
+        elements[2] = new Element(new Coordonnees(coordonnees.getAbscisse(), coordonnees.getOrdonnee() + 1), couleur);
+
+        // Quatrième élément : en bas à droite
+        elements[3] = new Element(new Coordonnees(coordonnees.getAbscisse() + 1, coordonnees.getOrdonnee() + 1), couleur);
+    }
+
+    // Retourner le tableau des éléments
+    public Element[] getElements() {
+        return elements;
+    }
+
+    // Afficher l'OTetromino sous forme de texte
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("OTetromino :\n");
+
+        // Pour chaque élément, afficher sa position et couleur
+        for (Element e : elements) {
+            sb.append("\t").append(e.toString()).append("\n");
+        }
+
+        return sb.toString();  // Retourne la chaîne
+    }
+}
